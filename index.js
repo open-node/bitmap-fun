c9689ec81283b331ef6c7b62069b9bf151b39ba8
Redstone Zhao <13740080@qq.com>
2019-12-11 17:53:28 +0800

    可以多函数

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const util = require("util");
 const jsonBody = util.promisify(require("body/json"));
 const Sequelize = require("sequelize");
-const Storage = require("../../src/storage/");
-const { store: opt } = require("../../configs/");
+const Storage = require("./src/storage/");
+const { store: opt } = require("./configs/");
 
 let store = null;
 
@@ -13,6 +13,22 @@ module.exports.initializer = async (context, callback) => {
   store = new Storage(sequelize);
   await store.init();
   callback();
+};
+
+module.exports.item = async (req, resp, context) => {
+  resp.setHeader("Content-Type", "Application/json");
+
+  console.log(JSON.stringify(context, null, 2));
+  const id = req.queries.id;
+
+  try {
+    const log = await store.item(id);
+    resp.setStatusCode(200);
+    resp.send(JSON.stringify(log));
+  } catch (e) {
+    resp.setStatusCode(500);
+    resp.send(e.message);
+  }
 };
 
 module.exports.save = async (req, resp, context) => {
